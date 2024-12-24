c4f494f976ded38e259f5604c0e52e4fe779dd1b
Sarunwit00 <a.sarunwit@gmail.com>
2024-12-24 15:09:47 +0700

    save

---
+++ app4/lib/screens/Home_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.amber,
+      body: ListView(
+        children: [
+          Container(
+            //width: 200,
+            height: 200,
+            color: Colors.deepPurple,
+            child: const Center(
+              child: Text("Test My app"),
+            ),
+          ),
+          // 2nd block
+          Container(
+            //width: 200,
+            height: 200,
+            color: Colors.deepPurple[400],
+            child: const Center(
+              child: Text("Test My app"),
+            ),
+          ),
+          // 3nd block
+          Container(
+            //width: 200,
+            height: 200,
+            color: Colors.deepPurple[200],
+            child: const Center(
+              child: Text("Test My app"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
